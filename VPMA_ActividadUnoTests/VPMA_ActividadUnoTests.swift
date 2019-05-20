0132d9edbb9bfe6377779d54c002ee33a3618cc2
MACBOOK <macbook@srmac7.local>
2019-05-20 10:26:35 -0500

    Initial Commit

---
+++ VPMA_ActividadUnoTests/VPMA_ActividadUnoTests.swift
@@ -0,0 +1,34 @@
+//
+//  VPMA_ActividadUnoTests.swift
+//  VPMA_ActividadUnoTests
+//
+//  Created by MACBOOK on 20/05/19.
+//  Copyright © 2019 MACBOOK. All rights reserved.
+//
+
+import XCTest
+@testable import VPMA_ActividadUno
+
+class VPMA_ActividadUnoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
