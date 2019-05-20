f66ef798a2da7dbc14bd6b5080777d2c6fb4dfe7
Abraham Vazquez <mvazquezp1000@alumno.ipn.mx>
2019-05-20 14:18:09 -0500

    Creación del Objeto empleado

--- VPMA_ActividadUno/Empleados.swift
+++ VPMA_ActividadUno/Empleados.swift
@@ -8,7 +8,23 @@
 
 import Foundation
 
-public class empleados
+public class Empleados
 {
+    public var nombre: String = ""
+    public var email:  String = ""
+    public var telefono: Int = 0
+    public var fechaNac: String = ""
+    public var drowssap: String = ""
+    public var reDrowssap: String = ""
     
+    public var Empleados: [Empleados] = []
+    
+    init(nombre: String, email: String, telefono: Int, fechaNac: String, drowssap: String, reDrowssap: String) {
+        self.nombre     = nombre
+        self.email      = email
+        self.telefono   = telefono
+        self.fechaNac   = fechaNac
+        self.drowssap   = drowssap
+        self.reDrowssap = reDrowssap
+    }
 }
