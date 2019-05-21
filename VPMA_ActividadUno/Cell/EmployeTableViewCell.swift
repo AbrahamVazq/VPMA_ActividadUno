43d244bc817508bcbe1c26cbe76dbce1433bc252
Abraham Vazquez <mvazquezp1000@alumno.ipn.mx>
2019-05-21 08:20:03 -0500

    Creación de Celda para la tabla de empleados

---
+++ VPMA_ActividadUno/Cell/EmployeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmployeTableViewCell.swift
+//  VPMA_ActividadUno
+//
+//  Created by Abraham Vazquez on 5/20/19.
+//  Copyright © 2019 MACBOOK. All rights reserved.
+//
+
+import UIKit
+
+class EmployeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
